c28eee9a2a35b9c62dbfbe98f1fc587d2d511c87
Duy Luong <duyluong@kms-technology.com>
2018-12-12 11:12:37 +0700

    remove FilterAdapter and add doc for PlatformException

--- src/main/java/com/katalon/platform/api/PlatformException.java
+++ src/main/java/com/katalon/platform/api/PlatformException.java
@@ -4,7 +4,36 @@ public class PlatformException extends Exception {
 
     private static final long serialVersionUID = 1L;
 
-    public PlatformException(Exception e) {
-        super(e);
+    private final String detailMessage;
+
+    private final Throwable rootCause;
+
+    public PlatformException(String message) {
+        this(message, null);
+    }
+
+    public PlatformException(Exception rootCause) {
+        this("", rootCause);
+    }
+
+    public PlatformException(String message, Exception rootCause) {
+        super(rootCause);
+        this.detailMessage = message;
+        this.rootCause = rootCause;
+    }
+
+    public String getDetailMessage() {
+        return detailMessage;
+    }
+
+    public Throwable getCause() {
+        return rootCause;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("PlatformException { detailMessage : %s, rootCause %s }",
+                detailMessage != null ? detailMessage : "null",
+                rootCause != null ? rootCause.toString() : "null");
     }
 }
