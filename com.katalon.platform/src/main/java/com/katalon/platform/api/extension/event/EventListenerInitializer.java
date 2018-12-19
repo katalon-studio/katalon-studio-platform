7d7dd60ae2a84a6597a5288d88ab4deca4c56f50
Thanh To <thanhto@kms.com.vn>
2018-12-19 15:40:13 +0700

    Remove redundant files

--- com.katalon.platform/src/main/java/com/katalon/platform/api/extension/event/EventListenerInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.katalon.platform.api.extension.event;
-
-public interface EventListenerInitializer {
-    void registerListener(EventListener listener);
-    void onInstall(String pluginId);
-    void onUninstall(String pluginId);
-}
