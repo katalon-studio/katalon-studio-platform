50b5df1deab18ed926bfdea37ccad50f44f7bd18
Thanh To <thanhto@kms.com.vn>
2018-12-21 14:25:37 +0700

    Removed redundant method declarations in ProjectEntity Interface

--- com.katalon.platform/src/main/java/com/katalon/platform/api/model/ProjectEntity.java
+++ com.katalon.platform/src/main/java/com/katalon/platform/api/model/ProjectEntity.java
@@ -1,12 +1,5 @@
 package com.katalon.platform.api.model;
 
 public interface ProjectEntity extends Entity {
-
-	public String getFileLocation();
-
-	public String getFolderLocation();
-
-	public String getId();
 	
-	public String getName();
 }
