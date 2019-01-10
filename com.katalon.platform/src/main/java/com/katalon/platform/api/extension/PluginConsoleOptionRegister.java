28e06b1147ed71c897407214edc2383090f3b84c
Thanh To <thanhto@kms.com.vn>
2019-01-10 12:09:51 +0700

    Added method documentations

--- com.katalon.platform/src/main/java/com/katalon/platform/api/extension/PluginConsoleOptionRegister.java
+++ com.katalon.platform/src/main/java/com/katalon/platform/api/extension/PluginConsoleOptionRegister.java
@@ -7,8 +7,22 @@ import com.katalon.platform.api.model.TestCaseEntity;
 
 public interface PluginConsoleOptionRegister {
 
+	/**
+	 * This method is intended to be implemented by clients to decide additional 
+	 * console options that will be recognized during console pre-execution
+	 * 
+	 * @return A list of console options
+	 */
 	List<PluginConsoleOption<?>> getPluginConsoleOptionList();
 	
-	void filterTestCasesOnPluginConsoleOptionRecognized(PluginConsoleOption<?> pluginConsoleOption, List<TestCaseEntity> testCases);
+	/**
+	 * This method is intended to be implemented by clients to select test cases they want to be executed
+	 * in a test suite
+	 * 
+	 * @param pluginConsoleOption A console option recognized during console pre-execution phase
+	 * @param testCases The list of test cases of the current to-be-launched test suites
+	 * @return A list of test cases that will be executed instead
+	 */
+	List<TestCaseEntity> filterTestCasesOnPluginConsoleOptionRecognized(PluginConsoleOption<?> pluginConsoleOption, List<TestCaseEntity> testCases);
 		
 }
