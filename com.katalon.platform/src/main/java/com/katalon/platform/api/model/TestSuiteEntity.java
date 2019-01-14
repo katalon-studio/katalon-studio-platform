86d691d9930e8a18c9c4365085af40b7542d316d
Thanh To <thanhto@kms.com.vn>
2019-01-14 13:43:37 +0700

    Remove redundant methods from TestSuiteEntity

--- com.katalon.platform/src/main/java/com/katalon/platform/api/model/TestSuiteEntity.java
+++ com.katalon.platform/src/main/java/com/katalon/platform/api/model/TestSuiteEntity.java
@@ -1,13 +1,8 @@
 package com.katalon.platform.api.model;
 
-import java.util.List;
 
 public interface TestSuiteEntity extends HasIntegration {
     String getParentFolderId();
 
     String getDescription();
-    
-    String getTag();
-    
-    List<String> getTestCaseIds();
 }
