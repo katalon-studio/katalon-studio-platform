2bbd10994a2cc8a289379925681b2691a8c0624a
Duy Luong <duyluong@kms-technology.com>
2019-01-15 19:09:40 +0700

    Add javadoc for PluginPreferencePage, TestCaseIntegrationViewDescription, and ReportIntegrationViewDescription

--- com.katalon.platform/src/main/java/com/katalon/platform/api/extension/PluginPreferencePage.java
+++ com.katalon.platform/src/main/java/com/katalon/platform/api/extension/PluginPreferencePage.java
@@ -2,12 +2,55 @@ package com.katalon.platform.api.extension;
 
 import org.eclipse.jface.preference.PreferencePage;
 
+/**
+ * PluginPreferencePage is the interface of <code>com.katalon.platform.api.extension.pluginPreferencePage</code>
+ * extension point that allows client plugins can add a Plugin Setting page under <b>Plugins</b> category in <b>Project
+ * Settings</b> dialog.
+ * <p>
+ * Register in the <code>plugin.xml</code> like this:
+ * 
+ * <pre>
+ * {@code
+ * <extension
+ *        point="com.katalon.platform.extensions">
+ *     <point
+ *           id="<your extension id>"
+ *           extensionPointId="com.katalon.platform.api.extension.pluginPreferencePage"
+ *           implementationClass="<name of your implementation class>"
+ *     </point>
+ * </extension>
+ * }
+ * </pre>
+ * 
+ * The <i>implementationClass</i> is the full qualified name of the class that implements
+ * PluginPreferencePage.
+ * 
+ * @since 1.0.4
+ *
+ */
 public interface PluginPreferencePage {
+    /**
+     * Id of this extension point
+     * 
+     * @since 1.0.4
+     */
     String EXTENSION_POINT_ID = "com.katalon.platform.api.extension.pluginPreferencePage";
 
+    /**
+     * @return Name of the plugin page
+     * @since 1.0.4
+     */
     String getName();
 
+    /**
+     * @return Id of the plugin page
+     * @since 1.0.4
+     */
     String getPageId();
 
+    /**
+     * @return clazz that extends PreferencePage
+     * @since 1.0.4
+     */
     Class<? extends PreferencePage> getPreferencePageClass();
 }
--- com.katalon.platform/src/main/java/com/katalon/platform/api/extension/TestCaseIntegrationViewDescription.java
+++ com.katalon.platform/src/main/java/com/katalon/platform/api/extension/TestCaseIntegrationViewDescription.java
@@ -6,24 +6,97 @@ import org.eclipse.swt.widgets.Control;
 import com.katalon.platform.api.model.ProjectEntity;
 import com.katalon.platform.api.model.TestCaseEntity;
 
+/**
+ * TestCaseIntegrationViewDescription is the interface of
+ * <code>com.katalon.platform.api.extension.testCaseIntegrationViewDescription</code>
+ * extension point that allows client plugins can add a view in Test Case integration tab.
+ * <p>
+ * Register in the <code>plugin.xml</code> like this:
+ * 
+ * <pre>
+ * {@code
+ * <extension
+ *        point="com.katalon.platform.extensions">
+ *     <point
+ *           id="<your extension id>"
+ *           extensionPointId="com.katalon.platform.api.extension.testCaseIntegrationViewDescription"
+ *           implementationClass="<name of your implementation class>"
+ *     </point>
+ * </extension>
+ * }
+ * </pre>
+ * 
+ * The <i>implementationClass</i> is the full qualified name of the class that implements
+ * TestCaseIntegrationViewDescription.
+ * 
+ * @since 1.0.4
+ *
+ */
 public interface TestCaseIntegrationViewDescription {
+    /**
+     * Id of this extension point
+     * 
+     * @since 1.0.4
+     */
     String EXTENSION_POINT_ID = "com.katalon.platform.api.extension.testCaseIntegrationViewDescription";
 
+    /**
+     * @param projectEntity the working project
+     * @return true if this view is visible. Otherwise, false.
+     * @since 1.0.4
+     */
     default boolean isEnabled(ProjectEntity projectEntity) {
         return true;
     }
-    
+
+    /**
+     * @return Name of integration view
+     * @since 1.0.4
+     */
     String getName();
 
+    /**
+     * @return clazz that implements TestCaseIntegrationView
+     * @since 1.0.4
+     */
     Class<? extends TestCaseIntegrationView> getTestCaseIntegrationView();
 
+    /**
+     * Describes how the integration view is displayed.
+     * 
+     * @since 1.0.4
+     */
     public interface TestCaseIntegrationView {
+        /**
+         * Creates the integration views.
+         * 
+         * @param parent parent view of the integration view
+         * @param partActionService utility service that helps to interact with Test Case view.
+         * @param testCase the working test case
+         * @return the created integration view
+         * @since 1.0.4
+         */
         Control onCreateView(Composite parent, PartActionService partActionService, TestCaseEntity testCase);
     }
 
+    /**
+     * A utility that helps the integration view can interact with Test Case view.
+     * 
+     * @since 1.0.4
+     */
     public interface PartActionService {
+        /**
+         * Marks the test case view is able to save.
+         * 
+         * @since 1.0.4
+         */
         void markDirty();
 
+        /**
+         * Checks the test case view is able to save or not.
+         * @return true if test case view needs to save. Otherwise, false.
+         * @since 1.0.4
+         */
         boolean isDirty();
     }
 }
