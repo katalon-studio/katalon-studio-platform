d19f77a8e612aafa0169a9b523babde83a5ebae4
Thanh To <thanhto@kms.com.vn>
2019-01-23 14:23:32 +0700

    Done KAT-3989

--- com.katalon.platform/src/main/java/com/katalon/platform/api/controller/TestObjectController.java
+++ com.katalon.platform/src/main/java/com/katalon/platform/api/controller/TestObjectController.java
@@ -1,15 +1,19 @@
 package com.katalon.platform.api.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import com.katalon.platform.api.exception.ResourceException;
 import com.katalon.platform.api.model.FolderEntity;
 import com.katalon.platform.api.model.ProjectEntity;
 import com.katalon.platform.api.model.TestObjectEntity;
+import com.katalon.platform.api.model.TestObjectEntity.SelectorMethod;
 import com.katalon.platform.api.model.TestObjectEntity.TestObjectProperty;
 import com.katalon.platform.api.model.TestObjectEntity.TestObjectXPath;
 
 public interface TestObjectController extends Controller {
+	
+	List<TestObjectEntity> getTestObjects(ProjectEntity projectEntity) throws ResourceException;
 	
 	TestObjectEntity getTestObject(ProjectEntity projectEntity, String testObjectId) throws ResourceException;
 	
@@ -19,9 +23,15 @@ public interface TestObjectController extends Controller {
 	
 	
 	public interface TestObjectDescription {
+		
+		String testObjectId();
+		
 		List<TestObjectProperty> getProperties();
 		
 		List<TestObjectXPath> getXPaths();
-	}
+		
+		Map<SelectorMethod, String> getSelectorCollection();
 	
+	}	
+
 }
