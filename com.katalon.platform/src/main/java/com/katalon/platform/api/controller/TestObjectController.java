bfca45e599f6ef3e7a6171862752011670ecaf92
Thanh To <thanhto@kms.com.vn>
2019-01-23 19:13:49 +0700

    Remove unused method declarations in TestObjectController

--- com.katalon.platform/src/main/java/com/katalon/platform/api/controller/TestObjectController.java
+++ com.katalon.platform/src/main/java/com/katalon/platform/api/controller/TestObjectController.java
@@ -1,37 +1,14 @@
 package com.katalon.platform.api.controller;
 
 import java.util.List;
-import java.util.Map;
-
 import com.katalon.platform.api.exception.ResourceException;
-import com.katalon.platform.api.model.FolderEntity;
 import com.katalon.platform.api.model.ProjectEntity;
 import com.katalon.platform.api.model.TestObjectEntity;
-import com.katalon.platform.api.model.TestObjectEntity.SelectorMethod;
-import com.katalon.platform.api.model.TestObjectEntity.TestObjectProperty;
-import com.katalon.platform.api.model.TestObjectEntity.TestObjectXPath;
 
 public interface TestObjectController extends Controller {
 	
 	List<TestObjectEntity> getTestObjects(ProjectEntity projectEntity) throws ResourceException;
 	
 	TestObjectEntity getTestObject(ProjectEntity projectEntity, String testObjectId) throws ResourceException;
-	
-	TestObjectEntity newTestObject(ProjectEntity projectEntity, FolderEntity parentFolder, String newTestObjectName) throws ResourceException;
-	
-	TestObjectEntity updateTestObject(ProjectEntity projectEntity, FolderEntity parentFolder, TestObjectDescription newTestObject) throws ResourceException;
-	
-	
-	public interface TestObjectDescription {
-		
-		String testObjectId();
-		
-		List<TestObjectProperty> getProperties();
-		
-		List<TestObjectXPath> getXPaths();
-		
-		Map<SelectorMethod, String> getSelectorCollection();
-	
-	}	
 
 }
