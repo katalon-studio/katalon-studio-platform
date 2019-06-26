a105547e4b3d2b4f2094dd081d1e858379c24e78
Huy Nguyen <huynguyen@SGH034RY6T.kms.com.vn>
2019-06-26 14:54:27 +0700

    Add more action to DialogActionService

--- com.katalon.platform/src/main/java/com/katalon/platform/api/ui/DialogActionService.java
+++ com.katalon.platform/src/main/java/com/katalon/platform/api/ui/DialogActionService.java
@@ -4,6 +4,8 @@ import org.eclipse.swt.widgets.Shell;
 
 import com.katalon.platform.api.exception.PlatformException;
 import com.katalon.platform.api.model.FolderEntity;
+import com.katalon.platform.api.model.TestCaseEntity;
+import com.katalon.platform.api.model.TestObjectEntity;
 
 /**
  * UISynchronizeService is a unique KS UIService to help KS plugins can use KS dialog built-in functions.
@@ -26,6 +28,42 @@ public interface DialogActionService extends UIService {
      * @since 1.0.4
      */
     FolderEntity showTestCaseFolderSelectionDialog(Shell parentShell, String dialogTitle) throws PlatformException;
+
+    /**
+     * Opens the test object folder selection and returns the selected folder after users press OK button.
+     * 
+     * @param parentShell parent shell of the dialog.
+     * @param dialogTitle title of the dialog
+     * @return the selected folder
+     * @throws PlatformException thrown if KS could not initialize dialog and return the selected folder
+     * 
+     * @since 1.0.12
+     */
+    FolderEntity showTestObjectFolderSelectionDialog(Shell parentShell, String dialogTitle) throws PlatformException;
+
+    /**
+     * Opens the test case selection and returns the list of selected test cases after users press OK button.
+     * 
+     * @param parentShell parent shell of the dialog.
+     * @param dialogTitle title of the dialog.
+     * @return an array containing selected test cases.
+     * @throws PlatformException thrown if KS could not initialize dialog and return the selected test cases
+     * 
+     * @since 1.0.12
+     */
+    TestCaseEntity[] showTestCaseSelectionDialog(Shell parentShell, String dialogTitle) throws PlatformException;
+
+    /**
+     * Opens the test object selection and returns the list of selected test objects after users press OK button.
+     * 
+     * @param parentShell parent shell of the dialog.
+     * @param dialogTitle title of the dialog.
+     * @return an array containing selected test objects.
+     * @throws PlatformException thrown if KS could not initialize dialog and return the selected test objects
+     * 
+     * @since 1.0.12
+     */
+    TestObjectEntity[] showTestObjectSelectionDialog(Shell parentShell, String dialogTitle) throws PlatformException;
 
     /**
      * Opens KS preferences dialog
