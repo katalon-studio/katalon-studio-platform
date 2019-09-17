339dc6d70d5f3cec6a78910e5f3323da76853ee3
Huy Nguyen <huynguyen@SGH034RY6T.kms.com.vn>
2019-09-17 14:13:49 +0700

    issue3343: add method for register and deregister bundle

--- com.katalon.platform/src/main/java/com/katalon/platform/internal/api/PluginInstaller.java
+++ com.katalon.platform/src/main/java/com/katalon/platform/internal/api/PluginInstaller.java
@@ -8,4 +8,8 @@ public interface PluginInstaller {
     Bundle installPlugin(BundleContext bundleContext, String fileUrl) throws BundleException;
 
     Bundle uninstallPlugin(BundleContext bundleContext, String fileUrl) throws BundleException;
+    
+    Bundle register(Bundle bundle) throws BundleException;
+    
+    Bundle deregister(Bundle bundle) throws BundleException;
 }
